f2b599979e5df8a0ccbd0c4f0faff9e0558ca95a
Luis Miguel Garcia Rodríguez <siulcava7@gmail.com>
2019-09-08 12:26:14 +0200

    Añadido el trazador

--- README.md
+++ README.md
@@ -7,4 +7,8 @@ A partir de este paso se creara una rama develop que sera sobre la que se haran 
 
 Para el tratamiento de JSON usaremos la libreria de Google Gson. La importaremos a traves de maven como dependencia.
 
+A nivel pruebas de usuario de los servicios usamos la herramienta de escritorio Postman
+
 Para los test unitarios añadimos la libreria Mockito a traves de maven.
+
+Para añadir el trazador que se nos pasa como libreria, lo añadimos como dependencia al pom y añadimos los jar a nuestro repositorio local.
--- src/main/java/iteriam/sanitas/prueba/calculadora/service/CalculadoraServiceImpl.java
+++ src/main/java/iteriam/sanitas/prueba/calculadora/service/CalculadoraServiceImpl.java
@@ -2,20 +2,27 @@ package iteriam.sanitas.prueba.calculadora.service;
 
 import org.springframework.stereotype.Service;
 
+import io.corp.calculator.TracerImpl;
 import iteriam.sanitas.prueba.calculadora.dto.entrada.EntradaDTO;
 import iteriam.sanitas.prueba.calculadora.dto.salida.ResultadoDTO;
 
 @Service
 public class CalculadoraServiceImpl implements CalculadoraService {
 
+	TracerImpl trazador = new TracerImpl();
+
 	@Override
 	public ResultadoDTO suma(EntradaDTO entrada) {
-		return new ResultadoDTO(entrada.getNumero1() + entrada.getNumero2());
+		ResultadoDTO result = new ResultadoDTO(entrada.getNumero1() + entrada.getNumero2());
+		trazador.trace(result);
+		return result;
 	}
 
 	@Override
 	public ResultadoDTO resta(EntradaDTO entrada) {
-		return new ResultadoDTO(entrada.getNumero1() - entrada.getNumero2());
+		ResultadoDTO result = new ResultadoDTO(entrada.getNumero1() - entrada.getNumero2());
+		trazador.trace(result);
+		return result;
 	}
 
 }
